fd02e8de0509fe88f18d439996925e0f23d2cf9a
Shaurya-Saini <shauryathemaster01@gmail.com>
2025-11-02 12:10:47 +0530

    feat: extension prototype

--- TEMPLATE/Chrome Extension/content.js
+++ TEMPLATE/Chrome Extension/content.js
@@ -41,4 +41,63 @@ console.log('EmailClassifier loaded');
             })
         }
     }
+
+    async function loadModel(){
+        const model = await tf.loadLayersModel(chrome.runtime.getURL('model/model.json'));
+        return model;
+    }
+
+    try {
+        const tokenizerConfig = await loadTokenizerConfig();
+        tokenizer = new Tokenizer(tokenizerConfig);
+        model = await loadModel();
+    } catch (e){
+        console.error("error loading the tokenizer or model",e);
+        return;
+    }
+
+    async function watiForEmailRows(){
+        let rows;
+        for (let i=0;i<30;i++){
+            rows = document.querySelectorAll('tr.zA');
+            if (rows.length >0){
+                return rows;
+            }
+            await sleep(500);
+        }
+        throw new Error("no emails found");
+    }
+
+    async function classifyandHighlight(){
+        const rows = document.querySelectorAll('tr.zA');
+        for (const row of rows){
+            const senderElement = row.querySelector('.yX.xY .yP.xY .zF');
+            const subjectElement = row.querySelector('.y6 span[id] , .y6 span:not([id])');
+            if (!senderElement || !subjectElement){
+                continue;
+            }
+            const sender = senderElement.textContent.trim();
+            const subject = subjectElement.textContent.trim();
+            const inputText = sender + " " + subject;
+
+            const seq = tokenizer.textstoSequences([inputText]);
+            const paddedSeq = tokenizer.padSequences(seq);
+            const input = tf.tensor2d(paddedSeq, [1, tokenizer.maxLen]);
+            let prediction,data;
+            try {
+                pred = model.predict(input);
+                data = await pred.data();
+            } catch (e){
+                console.error("error during prediction",e);
+                input.dispose();
+            }
+            if (data[0]<0.5){
+                row.slyle.background = '#ff0000'
+            }
+        }
+    }
+
+    await watiForEmailRows();
+    classifyandHighlight();
+
 })();
